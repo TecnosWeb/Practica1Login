2814d21df3273df4b2380db0e582d3eeb47d6008
Didier <dr.acula.belowme@gmail.com>
2015-04-28 23:37:36 -0500

    login que entregamos

---
+++ P1Login/src/controlador/Gestionar.java
@@ -0,0 +1,84 @@
+package controlador;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+	public class Gestionar{
+
+		public boolean buscarUsuario (String nombreUsuario,String pass) throws FileNotFoundException{
+			//boolean resultado=false; 			
+			System.out.println("entró a buscar");
+			    try {
+			    	BufferedReader br = new BufferedReader(new FileReader("datos.txt"),2);
+			    	String line = br.readLine();
+					String[] lineaSeparada;
+
+					while (line != null){
+						lineaSeparada = line.split("\\t");
+						System.out.println(line);
+						if (lineaSeparada[0].equals(nombreUsuario)&&lineaSeparada[1].equals(pass)) {
+							br.close();
+							return true;
+			        		}
+						line=br.readLine();
+			        }
+			        br.close();
+			    } catch (IOException e) {
+					e.printStackTrace();
+					System.out.println("Error en la busqueda");
+				}
+			return false;
+		}
+		
+		public Gestionar() {
+			// TODO Auto-generated constructor stub
+		}
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+		}
+		public File crearDirectorio(){
+			File f = new File("datos.txt");
+			//System.out.println("Creacion de directorio correcta");
+			return f;
+		}
+		
+		public boolean registrarUsuario(File f,String user, String password, String nombre, String apellidos, String email) {	
+			String data=user + "\t" + password + "\t" + nombre + "\t" + apellidos+ "\t" + email;
+			   File aux=f; 
+			   FileWriter wArchivo; 
+			   try{ 
+			       wArchivo = new FileWriter(aux,true); 
+			       BufferedWriter bw = new BufferedWriter(wArchivo,2);
+			       PrintWriter pw = new PrintWriter(bw); 
+			       pw.println(data);
+			       bw.close(); 
+			       return true;
+				}  
+			    catch(Exception e) 
+			    { 
+			            e.printStackTrace(); 
+			    } 
+			   return false;
+	    }
+		public void respuesta(HttpServletResponse resp, String msg) throws IOException {
+			PrintWriter out = resp.getWriter();
+			out.println("<html>");
+			out.println("<body>");
+			out.println("<t1>" + msg + "</t1>");
+			out.println("</body>");
+			out.println("</html>");
+		}
+		private boolean tieneNombreYDominio(String Email) {
+			String[] tokens = Email.split("@");
+			return tokens.length == 2;
+		}
+}
